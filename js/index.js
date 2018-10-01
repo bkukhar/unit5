a8c85ca0dfa13b23ee4dd10f7b7db849105c3127
Bogdan <dan.kukhar@gmail.com>
2018-10-01 14:18:12 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -1,21 +1,52 @@
-function solveQuadrEq(a, b, c){
+// const userName = prompt('Enter your name');
+// const greetings = 'Hello ' + userName;
+// alert(greetings);
+
+// const userAge = parseInt(prompt ('Enter your age'));
+// const nextAge = userAge + 1;
+// alert ('Next year you will be ' + nextAge);
+
+// const userBirthNumber = parseInt(prompt ('Enter month of birth number'));
+// const halfTime = userBirthNumber / 2;
+// alert ('Half of your month number is ' + halfTime);
+
+// const userAge = parseInt(prompt ('Enter your age'));
+// if (userAge >= 18) { // < > <= >= == === != !== - нерівність
+//     alert('You are adult');
+// } else {
+//     alert('You are child');
+// }
+
+// function addOne(num) {
+// const newNum = num + 1;
+// return newNum;
+// }
+
+// let result = addOne(10);
+// alert(result);
+// result = addOne(result);
+// alert(result);
+
+// result = Math.sqrt(16);
+// alert(result);
+// alert(result);
+
+// питаєм у користувача a b c
+
+
+function solveQuadrEq(a, b, c) {
     const d = calcDiscr(a, b, c);
     if (d > 0) {
-        const x1 = ((-b - Math.sqrt( d )) / ( 2 * a ));//
-        const x2 = ((-b + Math.sqrt( d )) / ( 2 * a ));//
+        const x1 = (-b - Math.sqrt(d)) / (2 * a);
+        const x2 = (-b + Math.sqrt(d)) / (2 * a);
         return 'The result is\nX1 = ' + x1 + '\nX2 = ' + x2;
-    }
-    else if (d == 0) {
-        const x = (-b) / (2 * a);
-        return x;
-    }
-    else {
-        return 'Sorry, no results!';
-    }
+    }   else if (d == 0) {
+        return 'The result is X = ' + -b / 2 * a;
+    }   return 'Sorry, no results!';
 }
 
-function calcDiscr(a, b, c){
-    const d = b*b - 4*a*c;
+function calcDiscr(a, b, c) {
+    const d = b * b - 4 * a * c;
     return d;
 }
 
